5bcacc5bcbeba50eebd82d7ae48c7c916d5d9f1b
zigstum <zigstum@gmail.com>
2016-06-16 02:10:04 +0200

    Deleted some files...

--- scripts/3greet.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-. /usr/local/lib/7dtd/common.sh
-
-telnetCommand $1 "sayplayer $4 \"[FFD700]Welcome to 7N2L $3, a PVE ONLY server. MINIBIKES ARE BANNED! If you spawn with no items, ask an admin for assistance in main chat.[-]\""
-
-#Is it the resident streamer?
-#if [ $3 == 76561198047552699 ]; then
-#telnetCommand $1 "say \"[FFD700]Sweep your path and cut your grass - Mouse is in the House![-]\""
--- scripts/test.sh
+++
@@ -1,3 +0,0 @@
-type="$(php -f /home/sdtd/instances/7n2l/hooks/scripts/phpres.sh 76561198055041232)"
-echo "$type" >> /root/unfied.txt
-exit 0
